f671c1d530184c19e1435a0d1102b9ab4f05091d
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2022-05-22 16:06:56 -0400

    format app.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import RightImage from "./RightImage";
 function App() {
   const [stage, setStage] = React.useState(0);
   const [answers, setAnswers] = React.useState({
-    0: '',
-    1: '',
-    2: ''
+    0: "",
+    1: "",
+    2: "",
   });
 
   if (stage === 3) {
-    alert('Form Data: ' + JSON.stringify(answers))
-}
+    alert("Form Data: " + JSON.stringify(answers));
+  }
 
   return (
     <div className="stage">
-      <LeftContainer stage={stage} setStage={setStage} answers={answers} setAnswers={setAnswers}/>
-      <RightImage stage={stage}/>
+      <LeftContainer
+        stage={stage}
+        setStage={setStage}
+        answers={answers}
+        setAnswers={setAnswers}
+      />
+      <RightImage stage={stage} />
     </div>
   );
 }
